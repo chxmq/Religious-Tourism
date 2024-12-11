55e7e806322cf10b667a11e79cc73a178e035ed3
Zxid-Shxrieff <reachzaids@gmail.com>
2024-12-12 00:22:03 +0530

    Create Static Itinerary.js

--- src/pages/Itinerary.js
+++ src/pages/Itinerary.js
@@ -1,7 +1,11 @@
-
+import React from 'react';
 
 function Itinerary() {
-  
+    return (
+        <div style={{ textAlign: 'center', marginTop: '20px' }}>
+            <h1 style={{ fontSize: '2rem', color: '#333' }}>Create Itinerary with Artilect AI</h1>
+        </div>
+    );
 }
 
 export default Itinerary;
