df30c034e5f261f812610d44e2abf84d282434d1
mahanteshpb1 <131250153+mahanteshpb1@users.noreply.github.com>
2024-12-29 21:35:55 +0530

    Update Itinerary.js

--- src/pages/Itinerary.js
+++ src/pages/Itinerary.js
@@ -1,18 +1,7 @@
-<<<<<<< HEAD
-import React from 'react';
-
-function Itinerary() {
-    return (
-        <div style={{ textAlign: 'center', marginTop: '20px' }}>
-            <h1 style={{ fontSize: '2rem', color: '#333' }}>Create Itinerary with Artilect AI</h1>
-        </div>
-    );
-=======
 
 
 function Itinerary() {
   
->>>>>>> 5ac5a10 (first commit)
 }
 
 export default Itinerary;
