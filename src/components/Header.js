c9e1d6ec28b075a43fda39284bcef5e41fda3b4a
mahanteshpb1 <131250153+mahanteshpb1@users.noreply.github.com>
2024-12-29 22:04:52 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,41 +1,5 @@
 import React, { useEffect, useState } from "react";
 
-<<<<<<< HEAD
-function Header({ showImages }) {
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
-
-  const images = [
-    "/assets/images/ladakh.jpg",
-    "/assets/images/himalay1.jpg",
-    "/assets/images/tajmahal2.jpg",
-    "/assets/images/himalay2.jpg",
-    "/assets/images/mysore.jpg",
-    "/assets/images/tajmahal1.jpg",
-    "/assets/images/temple1.jpg",
-  ];
-
-  useEffect(() => {
-    if (showImages) {
-      const changeImage = () => {
-        setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
-      };
-
-      const interval = setInterval(changeImage, 3000);
-
-      return () => clearInterval(interval);
-    }
-  }, [showImages, images.length]);
-
-  return (
-    <header
-      id="header"
-      style={{
-        backgroundImage: showImages
-          ? `url(${images[currentImageIndex]})`
-          : "none", // Hide background images when false
-      }}
-    ></header>
-=======
 
 function Header() {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
@@ -119,7 +83,6 @@ function Header() {
 
       </div>
       
->>>>>>> 5ac5a10 (first commit)
   );
 }
 
