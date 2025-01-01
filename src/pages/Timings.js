8e2b7b2dfa513d7b6cb7242e9d5f2fd6ecfc3ab0
Zxid-Shxrieff <reachzaids@gmail.com>
2025-01-01 22:19:24 +0530

    Update Timings.js

--- src/pages/Timings.js
+++ src/pages/Timings.js
@@ -1,7 +1,13 @@
-
+import React from 'react';
 
 function Timings() {
- 
+  return (
+      <div style={{ textAlign: 'center', marginTop: '20px' }}>
+          <h1 style={{ fontSize: '2rem', color: '#333', fontWeight: 'bold' }}>
+              Timings
+          </h1>
+      </div>
+  );
 }
 
 export default Timings;
