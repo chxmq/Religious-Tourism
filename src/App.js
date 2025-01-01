24fdb1da91dcca487ed3240e33c9d51730089692
Zxid-Shxrieff <reachzaids@gmail.com>
2025-01-01 22:50:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Header from "./components/Header";  
+import Header from "./components/Header";
+import Home from "./pages/Home";
 import Booking from "./pages/Booking";
 import Timings from "./pages/Timings";
 import Itinerary from "./pages/Itinerary";
 import Chatbox from "./components/Chatbox";
+import Chat from "./pages/Chat";
 import TourismCards from "./components/TourismCards";
 
 function App() {
   return (
     <Router>
-      <Navbar />
-      <Header />  {/* Include the Header component here */}
+      <Navbar />  {/* Include the Header component here */}
       <Routes>
-        <Route path="/" element={<TourismCards />} /> {/* Directly render TourismCards */}
-        <Route path="/booking" element={<Booking />} />
-        <Route path="/timings" element={<Timings />} />
-        <Route path="/itinerary" element={<Itinerary />} />
+        <Route
+          path="/"
+          element={
+            <>
+              <Header showImages={true} />  {/* Pass prop to show images */}
+              <TourismCards />} /> {/* Directly render TourismCards */}
+              <Home />
+            </>
+          }
+        />
+        <Route 
+          path="/booking" 
+          element={
+            <>
+              <Header showImages={false} />
+              <Booking />
+            </>
+          }
+        />
+        <Route 
+          path="/timings" 
+          element={
+            <>
+              <Header showImages={false} />
+              <Timings />
+            </>
+          }
+        />
+        <Route 
+          path="/itinerary" 
+          element={
+            <>
+              {/* Pass false to hide images */}
+              <Header showImages={false} />
+              <Itinerary />
+              <Chatbox />
+            </>
+          }
+        />
+        <Route 
+          path="/Chatbox" 
+          element={
+            <>
+              {/* Pass false to hide images */}
+              <Header showImages={false} />
+              <Chat />
+              <Chatbox />
+            </>
+          }
+        />
       </Routes>
-      <Chatbox />
     </Router>
   );
 }
