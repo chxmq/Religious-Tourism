2940a2b64dcd1800c689b10140c0bea533b13fc8
Zxid-Shxrieff <reachzaids@gmail.com>
2025-01-01 22:21:30 +0530

    Create Chat.js

---
+++ src/pages/Chat.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./Chat.css";
+
+function Chat() {
+    return (
+        <div style={{ textAlign: 'center', marginTop: '20px' }}>
+            <h1 style={{ fontSize: '2rem', color: '#333', fontWeight: 'bold' }}>
+                Welcome To Artilect AI Chatbox
+            </h1>
+            <p style={{ fontWeight: 'bold' }}>Ask your queries about religious tourism, and I'll help you out!
+            </p>
+        </div>
+    );
+}
+
+export default Chat;
